5dfdcdfb8ee65cc9a7b75ec5dee9da63fea6761a
Madhuj275 <madhuj.2004@yahoo.com>
2025-02-28 18:21:50 +0530

    Solved: Is Subsequence on Friday, 28 February 2025

--- solutions/is-subsequence.py
+++ solutions/is-subsequence.py
@@ -4,12 +4,10 @@
 
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        count=0
-        for i in s:
-            if i in t:
-                count+=1
-            else:
-                return False
-        if count==len(s):
-            return True
+        i = j = 0
+        while i < len(t) and j < len(s):
+            if t[i] == s[j]:
+                j += 1
+            i += 1
+        return j == len(s)
         
