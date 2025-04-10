def0c3cfba436de61b6fa80b7092336216049d56
Madhuj Agrawal <madhuj.2004@yahoo.com>
2025-04-11 01:23:36 +0530

    Time: 37 ms (84.06%), Space: 20.2 MB (57.04%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        max_profit = 0
+        j=0 
+        pr=0
+        temp =[]
+        
+        for i in range(len(profit)):
+            temp.append((difficulty[i], profit[i]))  
+        temp.sort()
+        worker.sort()
+
+        for w in worker:
+            while j < len(temp) and w >= temp[j][0]:
+                pr=max(pr,temp[j][1])
+                j+=1
+            max_profit +=pr
+        
+        return max_profit
+        
