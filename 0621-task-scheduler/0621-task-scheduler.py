3fdfd33d9fda9211b26d46e18ef3433ec5abf0c5
Madhuj Agrawal <madhuj.2004@yahoo.com>
2025-04-23 19:30:36 +0530

    Time: 47 ms (77.03%), Space: 13.8 MB (17.85%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,11 @@
+class Solution:
+    def leastInterval(self, tasks, n):  
+        if n == 0:
+            return len(tasks)
+        
+        count = Counter(tasks)
+        maxi = max(count.values())
+        max_tasks = sum( ( 1 for task, val in count.items() if val == maxi ) )
+        res = ( maxi-1 )*( n+1 ) + max_tasks
+        
+        return max(len(tasks), res)
