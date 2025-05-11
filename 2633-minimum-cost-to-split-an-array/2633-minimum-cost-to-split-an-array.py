26d53fe4194bfde64b69fb816e705ef316c38ae5
Madhuj Agrawal <madhuj.2004@yahoo.com>
2025-05-11 12:40:35 +0530

    Time: 2662 ms (63.36%), Space: 18 MB (77.64%) - LeetHub

---
+++ 2633-minimum-cost-to-split-an-array/2633-minimum-cost-to-split-an-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minCost(self, nums: List[int], k: int) -> int:
+        result = [0]
+        nums = [-1] + nums
+
+        def compute_cost(position):
+            frequency = defaultdict(int)
+            accumulated_cost = 0
+            minimum_cost = float('inf')
+            while position > 0:
+                if frequency[nums[position]] > 0:
+                    accumulated_cost += 1
+                    if frequency[nums[position]] == 1:
+                        accumulated_cost += 1
+                frequency[nums[position]] += 1
+                minimum_cost = min(result[position - 1] + accumulated_cost, minimum_cost)
+                position -= 1
+            return minimum_cost + k
+
+        for pos in range(1, len(nums)):
+            result.append(compute_cost(pos))
+
+        return result[-1]
