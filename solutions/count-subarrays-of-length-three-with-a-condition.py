b17677b0fd0f728206453c89589119ba47891797
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:29:34 +0530

    Add solution for count-subarrays-of-length-three-with-a-condition

---
+++ solutions/count-subarrays-of-length-three-with-a-condition.py
@@ -0,0 +1,15 @@
+# Problem: Count Subarrays of Length Three With a Condition
+# Difficulty: Unknown
+# Solution:
+
+import math
+
+class Solution(object):
+    def countSubarrays(self, nums):
+        count = 0
+        for i in range(len(nums) - 2):
+            left = nums[i] + nums[i+2]
+            right =(float(nums[i+1]) / 2)  
+            if left == right:
+                count += 1
+        return count
