193761128591767e615310aa30df9353a9493b70
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:31:44 +0530

    Add solution for minimum-operations-to-make-a-uni-value-grid

---
+++ solutions/minimum-operations-to-make-a-uni-value-grid.py
@@ -0,0 +1,24 @@
+# Problem: Minimum Operations to Make a Uni-Value Grid
+# Difficulty: Unknown
+# Solution:
+
+import math
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        mat=[num for row in grid for num in row]
+        if len(mat)<=1:
+            return 0
+        mat.sort()
+        count=0
+        a=math.ceil(mat[-1]/2)
+        for i in mat:
+            diff = abs(i - a)
+            if diff % x != 0:
+                return -1
+            count += diff // x
+        return count
+
+
+
+
+        
