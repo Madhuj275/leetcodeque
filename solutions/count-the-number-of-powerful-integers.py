903350c8550ab1de6e25e9cba74452abb93c4805
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:31:36 +0530

    Add solution for count-the-number-of-powerful-integers

--- solutions/count-the-number-of-powerful-integers.py
+++ solutions/count-the-number-of-powerful-integers.py
@@ -3,42 +3,22 @@
 # Solution:
 
 class Solution:
-    def numberOfPowerfulInt(self, a: int, b: int, lim: int, s: str) -> int:
-        def ok(x):
-            x = str(x)
-            if not x.endswith(s):
-                return False
-            for d in x:
-                if int(d) > lim:
-                    return False
-            return True
+    def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
+        count = 0
 
-        def cnt(x):
-            s_x = str(x)
-            n = len(s_x)
-            dp = {}
+        for num in range(start, finish + 1):
+            num_str = str(num)
+            valid = True
 
-            def go(i, tight, zero):
-                k = (i, tight, zero)
-                if k in dp:
-                    return dp[k]
-                if i == n:
-                    return 0 if zero else 1
-                res = 0
-                up = int(s_x[i]) if tight else 9
-                for d in range(0, up + 1):
-                    if d > lim:
-                        continue
-                    nt = tight and (d == up)
-                    nz = zero and (d == 0)
-                    res += go(i + 1, nt, nz)
-                dp[k] = res
-                return res
+            for ch in num_str:
+                if int(ch) > limit:
+                    valid = False
+                    break
 
-            return go(0, True, True)
+            if not num_str.endswith(s):
+                valid = False
 
-        ans = 0
-        for x in range(a, b + 1):
-            if ok(x):
-                ans += 1
-        return ans
+            if valid:
+                count += 1
+
+        return count
