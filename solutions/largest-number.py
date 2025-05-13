f6970d927172f02b87a3d6e44259cf82bf49908c
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:26:20 +0530

    Add solution for largest-number

--- solutions/largest-number.py
+++ solutions/largest-number.py
@@ -7,5 +7,5 @@ class Solution:
         digits = list(map(str, nums))
         digits.sort(reverse=True,key=lambda x: x * 10)
         result = ''.join(map(str, digits))
-        return '0' if result[0] == '0' else result
+        return result
         
