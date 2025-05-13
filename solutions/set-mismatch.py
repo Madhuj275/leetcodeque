8eb6d83ebe4f0e70c948862003b48557f92cb51e
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:27:04 +0530

    Add solution for set-mismatch

--- solutions/set-mismatch.py
+++ solutions/set-mismatch.py
@@ -3,13 +3,24 @@
 # Solution:
 
 class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        res=[]
-        for i in range(1,len(nums)):
-            if nums[i]==nums[i-1]:
-                res.append(nums[i])
-                res.append (i+1)
-        return res
+    def findErrorNums(self, arr: list[int]) -> list[int]:
+        n = len(arr)
+        seen = set()
+        duplicate = 0
+        missing = 0
+
+        for num in arr:
+            if num in seen:
+                duplicate = num
+            else:
+                seen.add(num)
+
+        for i in range(1, n + 1):
+            if i not in seen:
+                missing = i
+                break
+
+        return [duplicate, missing]
+
 
         
