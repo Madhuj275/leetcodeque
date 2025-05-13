97473ca412ef2d7e873cada47ee124ed563d1794
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:33:52 +0530

    Add solution for house-robber-iv

--- solutions/house-robber-iv.py
+++ solutions/house-robber-iv.py
@@ -8,8 +8,6 @@ class Solution:
         j=i+2
         tsum=0
         min_res=float('inf')
-        if k == 1:
-            return nums[0]
         while j < len(nums) :
             tsum = max(nums[i],nums[j])
             min_res=min(min_res,tsum)
