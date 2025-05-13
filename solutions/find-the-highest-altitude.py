b3a43f9e7b5777a782e60729755cc43115c784b1
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:32:34 +0530

    Add solution for find-the-highest-altitude

---
+++ solutions/find-the-highest-altitude.py
@@ -0,0 +1,14 @@
+# Problem: Find the Highest Altitude
+# Difficulty: Unknown
+# Solution:
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        res = [0] 
+        alt = 0 
+        for i in range(len(gain)):
+            alt += gain[i] 
+            res.append(alt) 
+        
+        maxi = max(res)  
+        return maxi
