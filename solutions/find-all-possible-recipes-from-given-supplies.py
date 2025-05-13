cb07d388c3bd2dd13b2895b1031f4c715a25a1a2
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:33:11 +0530

    Add solution for find-all-possible-recipes-from-given-supplies

---
+++ solutions/find-all-possible-recipes-from-given-supplies.py
@@ -0,0 +1,27 @@
+# Problem: Find All Possible Recipes from Given Supplies
+# Difficulty: Unknown
+# Solution:
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        check = {}
+        dic_ingre = defaultdict(list)
+
+        for i in range(len(ingredients)):
+            check[recipes[i]] = len(ingredients[i])
+            for j in range(len(ingredients[i])):
+                dic_ingre[ingredients[i][j]].append(recipes[i])
+
+        cur = defaultdict(int)
+        res = []
+
+        i = 0
+        while i < len(supplies):
+            for rec in dic_ingre[supplies[i]]:
+                cur[rec] += 1
+                if cur[rec] == check[rec]:
+                    res.append(rec)
+                    supplies.append(rec)
+            i += 1
+
+        return res
