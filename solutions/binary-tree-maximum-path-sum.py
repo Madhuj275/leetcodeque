d8eeca83abdb4f422e52883fdbb058dc9576cd77
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:30:45 +0530

    Add solution for binary-tree-maximum-path-sum

---
+++ solutions/binary-tree-maximum-path-sum.py
@@ -0,0 +1,34 @@
+# Problem: Binary Tree Maximum Path Sum
+# Difficulty: Unknown
+# Solution:
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int dfs(TreeNode* node, int &maxSum){
+    if(!node) return 0;
+
+    int leftSum = max(0, dfs(node->left, maxSum));
+    int rightSum = max(0, dfs(node->right, maxSum));
+
+    maxSum = max(maxSum, leftSum+rightSum+node->val);
+
+    return max(leftSum, rightSum) + node->val;
+  }
+
+  int maxPathSum(TreeNode* root) {
+    int maxSum = root->val;
+    dfs(root, maxSum);
+    return maxSum;
+  }
+};
