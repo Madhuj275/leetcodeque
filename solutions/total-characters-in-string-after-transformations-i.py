3373dd14fd377a9a55e2678007b4211f69a83f64
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:25:09 +0530

    Add solution for total-characters-in-string-after-transformations-i

--- solutions/total-characters-in-string-after-transformations-i.py
+++ solutions/total-characters-in-string-after-transformations-i.py
@@ -2,19 +2,25 @@
 # Difficulty: Unknown
 # Solution:
 
+from collections import Counter
+
 class Solution:
     def lengthAfterTransformations(self, s: str, t: int) -> int:
-        while t > 0:
-            result=''
-            for char in s:
-                if char == 'z':
-                    result += 'ab'
-                elif 'a' <= char <= 'y':
-                    result += chr(ord(char) + 1)
-                else:
-                    result += char
-            t-=1
-            s=result
-
-        return len(result)
+        MOD = 10**9 + 7
+        count = Counter(s)
+        length = len(s)
         
+        for _ in range(t):
+            new_count = [0] * 26
+            for i in range(25): 
+                new_count[i + 1] += count[chr(i + ord('a'))]
+            z_count = count['z']
+            if z_count > 0:
+                new_count[0] += z_count
+                new_count[1] += z_count
+                length += z_count  
+                
+            count = {chr(i + ord('a')): new_count[i] % MOD for i in range(26)}
+            length = length % MOD
+        
+        return length
