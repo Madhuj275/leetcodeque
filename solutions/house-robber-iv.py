462d8e35f46cb71418fafdaad24f4280007035af
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:33:49 +0530

    Add solution for house-robber-iv

--- solutions/house-robber-iv.py
+++ solutions/house-robber-iv.py
@@ -4,16 +4,17 @@
 
 class Solution:
     def minCapability(self, nums: List[int], k: int) -> int:
+        i=0
+        j=i+2
         tsum=0
         min_res=float('inf')
         if k == 1:
             return nums[0]
-        for i in range(len(nums)):
-            j = i + 2
-            while j < len(nums):
-                tsum = max(nums[i], nums[j])
-                min_res = min(min_res, tsum)
-                j += 1
+        while j < len(nums) :
+            tsum = max(nums[i],nums[j])
+            min_res=min(min_res,tsum)
+            j+=1
+        i+=1
 
         return min_res
 
