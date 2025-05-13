e3ad7d398f858630583cd9db4dcad4693df4d512
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:27:00 +0530

    Add solution for lemonade-change

--- solutions/lemonade-change.py
+++ solutions/lemonade-change.py
@@ -9,14 +9,12 @@ class Solution:
         for i in bills:
             if i == 5:
                 five+=1
-            elif i == 10:
-                if five >=1:
+            elif i ==10:
+                if five:
                     five-=1
                     ten+=1
-                else:
-                    return False
             else:
-                if ten >=1 and five >= 1:
+                if ten and five:
                     ten-=1
                     five-=1
                 elif five >=3:
