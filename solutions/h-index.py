e5f50211e0dc3acbd15ba7bb93fae10df8a490b5
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-13 13:26:04 +0530

    Add solution for h-index

--- solutions/h-index.py
+++ solutions/h-index.py
@@ -6,8 +6,6 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         h=0
         citations.sort(reverse=True)
-        if len(citations) == 1:
-            return nums[0]
         for i in range(len(citations)):
             if citations[i] >= i+1:
                 h+=1
