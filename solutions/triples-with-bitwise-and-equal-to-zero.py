2dee3cd6b7a9134fc5b8930fc42d4dd4e493f40d
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-14 12:14:01 +0530

    Add solution for triples-with-bitwise-and-equal-to-zero

---
+++ solutions/triples-with-bitwise-and-equal-to-zero.py
@@ -0,0 +1,19 @@
+# Problem: Triples with Bitwise AND Equal To Zero
+# Difficulty: Unknown
+# Solution:
+
+class Solution:
+    def countTriplets(self, nums: List[int]) -> int:
+        freq = defaultdict(int)
+        for x in nums: 
+            for y in nums: 
+                freq[x&y] += 1
+        
+        ans = 0
+        for x in nums: 
+            mask = x = x ^ 0xffff
+            while x: 
+                ans += freq[x]
+                x = mask & (x-1)
+            ans += freq[0]
+        return ans 
