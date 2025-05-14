66d5ee2ab2389c8179b02fc231550af69790e6f1
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-14 12:15:31 +0530

    Add solution for count-the-number-of-powerful-integers

--- solutions/count-the-number-of-powerful-integers.py
+++ solutions/count-the-number-of-powerful-integers.py
@@ -4,26 +4,24 @@
 
 class Solution:
     def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
-        def count(n):
-            digits = list(map(int, str(n)))
-            L = len(digits)
+        def dp(x):
+            digits = list(map(int, str(x)))
+            n = len(digits)
             suf_len = len(s)
-            suf = [int(x) for x in s]
-            dp = {}
+            suf_digits = list(map(int, s))
+            seen = {}
 
-            def dfs(i, tight, started, last_digits):
-                if i == L:
-                    if not started:
+            def f(i, tight, zero, path):
+                if i == n:
+                    if len(path) < suf_len:
                         return 0
-                    if len(last_digits) < suf_len:
-                        return 0
-                    if last_digits[-suf_len:] == suf:
+                    if path[-suf_len:] == suf_digits:
                         return 1
                     return 0
 
-                key = (i, tight, started, tuple(last_digits[-suf_len:]))
-                if key in dp:
-                    return dp[key]
+                key = (i, tight, zero, tuple(path[-suf_len:]))
+                if key in seen:
+                    return seen[key]
 
                 res = 0
                 max_d = digits[i] if tight else 9
@@ -31,16 +29,16 @@ class Solution:
                 for d in range(0, max_d + 1):
                     if d > limit:
                         continue
-                    new_tight = tight and (d == max_d)
-                    new_started = started or d != 0
-                    new_last_digits = last_digits.copy()
-                    if new_started:
-                        new_last_digits.append(d)
-                    res += dfs(i + 1, new_tight, new_started, new_last_digits)
+                    nt = tight and (d == max_d)
+                    nz = zero and d == 0
+                    if zero and d == 0:
+                        res += f(i + 1, nt, nz, path)
+                    else:
+                        res += f(i + 1, nt, False, path + [d])
 
-                dp[key] = res
+                seen[key] = res
                 return res
 
-            return dfs(0, True, False, [])
+            return f(0, True, True, [])
 
-        return count(finish) - count(start - 1)
+        return dp(finish) - dp(start - 1)
