16b5b1e3ae088b8d14fa67f6bb4745b7275fb4d1
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-15 21:32:54 +0530

    Add solution for count-largest-group

---
+++ solutions/count-largest-group.py
@@ -0,0 +1,27 @@
+# Problem: Count Largest Group
+# Difficulty: Unknown
+# Solution:
+
+class Solution(object):
+    def countLargestGroup(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count = {}
+
+        for i in range(1, n + 1):
+            x = i
+            dsum = 0
+            while x > 0:
+                dsum += x % 10
+                x = x // 10
+
+            if dsum not in count:
+                count[dsum] = []
+            count[dsum].append(i)
+        
+        max_len = max(len(val) for val in count.values())
+        res = {k: val for k, val in count.items() if len(val) == max_len}
+
+        return len(res)
