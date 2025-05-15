6ff1c23372c7f9250d8e8eff1ce35981f0d0e3e3
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-15 21:33:00 +0530

    Add solution for rabbits-in-forest

--- solutions/rabbits-in-forest.py
+++ solutions/rabbits-in-forest.py
@@ -11,10 +11,11 @@ class Solution(object):
         count = Counter(answers)
         res=0
         for ans in count:
-            gsize=ans+1
-            gcount=count[ans]
-            num_grps=(gcount + gsize - 1) // gsize
-            res += num_grps * gsize
+            if ans > 0:
+                res+=(ans+1)
+            else:
+                res+=(count[ans])
+        
         return res
             
 
