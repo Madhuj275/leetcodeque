1975ab00c25c7a610c6d928010a22afbc2d6685b
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-15 21:35:10 +0530

    Add solution for count-subarrays-of-length-three-with-a-condition

--- solutions/count-subarrays-of-length-three-with-a-condition.py
+++ solutions/count-subarrays-of-length-three-with-a-condition.py
@@ -3,13 +3,15 @@
 # Solution:
 
 import math
-
 class Solution(object):
     def countSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         count = 0
-        for i in range(len(nums) - 2):
-            left = nums[i] + nums[i+2]
-            right = math.ceil(float(nums[i+1]) / 2)  
-            if left == right:
+        for i in range(len(nums) - 2):  
+            if nums[i] + nums[i+2] == math.ceil(nums[i+1] / 2):
                 count += 1
+
         return count
