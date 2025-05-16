c376a61c5ea05ffd62469482625e84e9dfeb0111
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-16 19:38:03 +0530

    Add solution for count-complete-subarrays-in-an-array

---
+++ solutions/count-complete-subarrays-in-an-array.py
@@ -0,0 +1,24 @@
+# Problem: Count Complete Subarrays in an Array
+# Difficulty: Unknown
+# Solution:
+
+class Solution(object):
+    def countCompleteSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left=0
+        res=0
+        k=len(set(nums))
+        count={}
+        for i in range(len(nums)):
+            count[nums[i]] = count.get(nums[i], 0) + 1
+            while len(count) == k:
+                res += len(nums) - i
+                count[nums[left]] -= 1
+                if count[nums[left]] == 0:
+                    del count[nums[left]]
+                left += 1
+        return res
+
