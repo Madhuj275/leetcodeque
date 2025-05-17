6edee42ead7c0f75e74be2c60ae8a37679570541
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-18 02:14:06 +0530

    Add solution for arithmetic-slices

--- solutions/arithmetic-slices.py
+++ solutions/arithmetic-slices.py
@@ -6,6 +6,8 @@ class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         if len(nums) < 3:
             return 0
+        if len(nums)==3:
+            return 1
         prev=0
         count=0
         res=[]
