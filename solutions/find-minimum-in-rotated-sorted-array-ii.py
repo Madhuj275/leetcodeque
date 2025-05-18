4c095b374957744b22e8e7a709434d6c6f051bd1
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-19 00:11:41 +0530

    Add solution for find-minimum-in-rotated-sorted-array-ii

--- solutions/find-minimum-in-rotated-sorted-array-ii.py
+++ solutions/find-minimum-in-rotated-sorted-array-ii.py
@@ -4,5 +4,15 @@
 
 class Solution:
     def findMin(self, nums):
-        nums.sort()
-        return nums[0]
+        N=len(nums)
+        start=0
+        end=N-1
+        while start<end:
+            mid=(start+end)//2
+            while nums[mid]==nums[end] and mid<end:
+                end-=1
+            if nums[mid]>nums[end]:
+                start=mid+1
+            else:
+                end=mid
+        return nums[start]
