1fc0edec4d1c7a518cf41891477f56ef98dc6703
Madhuj275 <madhuj.2004@yahoo.com>
2025-05-18 23:31:40 +0530

    Add solution for longest-unequal-adjacent-groups-subsequence-ii

--- solutions/longest-unequal-adjacent-groups-subsequence-ii.py
+++ solutions/longest-unequal-adjacent-groups-subsequence-ii.py
@@ -4,13 +4,32 @@
 
 class Solution:
     def getWordsInLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
-        res = [words[0]]  
-        count=0
-        for i in range(1,len(words)):
-            if groups[i]!=groups[i-1] :
-                res.append(words[i])
-
-        return res
-
-
+        n = len(words)
+        if n == 1:
+            return [words[0]]
+        if all(g == groups[0] for g in groups):
+            return [max(words, key=len)]
         
+        memo = [[words[i]] for i in range(n)]
+        
+        for i in range(n):
+            for j in range(i):
+
+                if groups[i] != groups[j] and len(words[i]) == len(words[j]):
+                    count = 0
+                    for a, b in zip(words[i], words[j]):
+                        if a != b:
+                            count += 1
+                            if count > 1:
+                                break
+                    if count == 1 and len(memo[j]) + 1 > len(memo[i]):
+                        memo[i] = memo[j] + [words[i]]
+        
+        best = max(memo, key=len)
+        if len(best) > 1:
+            return best
+ 
+        max_group = max(groups)
+        for i in range(n):
+            if groups[i] == max_group:
+                return [words[i]]
