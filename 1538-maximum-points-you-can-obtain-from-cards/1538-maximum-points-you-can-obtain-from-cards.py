53b5cbee714f7b8965366ff3adbed36868917ae5
Madhuj Agrawal <madhuj.2004@yahoo.com>
2025-08-28 13:44:42 +0530

    Time: 34 ms (24.36%), Space: 28 MB (83.1%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -10,15 +10,13 @@ class Solution:
         suf = 0
         maxi = 0
 
-        # prefix: take from start
         for i in range(k):
             pre += cardPoints[i]
         maxi = pre
 
-        # suffix: slide backwards, reduce prefix, add suffix
-        for i in range(1, k+1):
-            pre -= cardPoints[k-i]
-            suf += cardPoints[n-i]
+        for i in range(n-1,n-k-1,-1):
+            pre -= cardPoints[i - (n-k)]
+            suf += cardPoints[i]
             maxi = max(maxi, pre + suf)
 
         return maxi
